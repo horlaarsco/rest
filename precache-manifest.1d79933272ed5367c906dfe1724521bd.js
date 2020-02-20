e0196711498f947db855d82e796d8ec09993e3dd
Darey <horla@Dareys-MacBook-Pro.local>
2020-02-20 01:00:30 +0100

    Updates

---
+++ precache-manifest.1d79933272ed5367c906dfe1724521bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e96b39abfa4cf47307f6e95a69a23c4",
+    "url": "/rest/index.html"
+  },
+  {
+    "revision": "d440a5a7da091a0de6b9",
+    "url": "/rest/static/css/main.abd371f0.chunk.css"
+  },
+  {
+    "revision": "921a000af62d0e467657",
+    "url": "/rest/static/js/2.1f494b78.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rest/static/js/2.1f494b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d440a5a7da091a0de6b9",
+    "url": "/rest/static/js/main.e821a1d6.chunk.js"
+  },
+  {
+    "revision": "cf76a6633d3bcab3c76f",
+    "url": "/rest/static/js/runtime-main.255ebe1b.js"
+  }
+]);
